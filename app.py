5b7e51b4d04dc6e7cee6732cbec2b9343a6bb8a6
Gain22916 <46260240+Gain22916@users.noreply.github.com>
2018-12-30 21:33:14 +0700

    Update app.py
    555

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
 from flask import Flask, request
 import json
 import requests
-
-
-global LINE_API_KEY
-LINE_API_KEY = 'Bearer H9zHxu1IDMdWw9/IlDTHnieguNyPrLBrVPriCOycpSHboHvlBUZeQ1y0bCco0eRU2ZknEUmxrM83E+ovUkBNnxpm+teQH5Pbtiifm8ZYp/nNXuSjjjIRLnLapQm96Rd6ux7u+A5nqfcnze2q+lYHKgdB04t89/1O/w1cDnyilFU='
-
-
 app = Flask(__name__)
- 
-@app.route("/")
-def hello():
-   return "Hello World!d : Gain Testing"
+@app.route('/')
+def index():
+  return "Hello World!"
 
-if __name__ == "__main__":
-  app.run()
+# ส่วน callback สำหรับ Webhook
+@app.route('/callback', methods=['POST'])
+def callback():
+  json_line = request.get_json()
+  json_line = json.dumps(json_line)
+  decoded = json.loads(json_line)
+  user = decoded["events"][0]['replyToken']
+  #id=[d['replyToken'] for d in user][0]
+  #print(json_line)
+  print("ผู้ใช้：",user)
+  sendText(user,'งง') # ส่งข้อความ งง
+  return '',200
+
+
+def sendText(user, text):
+  LINE_API = 'https://api.line.me/v2/bot/message/reply'
+  Authorization = 'Bearer H9zHxu1IDMdWw9/IlDTHnieguNyPrLBrVPriCOycpSHboHvlBUZeQ1y0bCco0eRU2ZknEUmxrM83E+ovUkBNnxpm+teQH5Pbtiifm8ZYp/nNXuSjjjIRLnLapQm96Rd6ux7u+A5nqfcnze2q+lYHKgdB04t89/1O/w1cDnyilFU=' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
+  headers = {
+  'Content-Type': 'application/json; charset=UTF-8',
+  'Authorization':Authorization
+  }
+  data = json.dumps({
+  "replyToken":user,
+  "messages":[{"type":"text","text":text}]})
+  #print("ข้อมูล：",data)
+  r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
+  #print(r.text)
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
